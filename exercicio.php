b389c3f87586fd004af85bd787b151bbcdd3fe9e
brandi170717 <emanuelly.brandi.senai404@gmail.com>
2024-08-07 16:46:09 -0300

    Update exercicio.php

--- exercicio.php
+++ exercicio.php
@@ -1,13 +1,13 @@
 <?php
-
+// primeiro defino as variáveis
 $nota1 = '9';
 $nota2 = '10';
 $nota3 = '8';
-
+// Depois chamo as variáveis
 echo "A primeira nota é = $nota1 <br>";
 echo "A segunda nota é = $nota2 <br>";
 echo "A terceira nota é = $nota3 <br>";
-
+// Faço a media
 $media = ($nota1 + $nota2 + $nota3)/3;
 
 echo "Sua nota é: $media <br>";
