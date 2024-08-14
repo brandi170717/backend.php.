905e55bc267c378913c66a8ae4de06b571877fab
brandi170717 <emanuelly.brandi.senai404@gmail.com>
2024-08-14 15:50:55 -0300

    Add files via upload

---
+++ operadores_ternarios.php
@@ -0,0 +1,23 @@
+<?php
+
+
+// Exemplo com variavel nome 
+$nome = "Emanuelly";
+$mensagem = ($nome == "Emanuelly") ? "Olá, Emanuelly." : "Olá, visitante.";
+echo $mensagem; // Saída: nome
+echo '<br>';
+
+
+// Exemplo com variavel numerico 
+$idade = 20;
+$mensagem = ($idade >=18) ? "Maior de idade." : "Menor de idade.";
+echo $mensagem; // Saída: idade
+echo '<br>';
+
+// Exemplo com variavel true 
+$is_logged_in = true;
+
+$mensagem = $is_logged_in ? 'Bem Vindo de volta!' : 'Por favor, faça login.';
+echo $mensagem; //Saída: True
+
+?>
---
+++ switch-case.php
@@ -0,0 +1,22 @@
+<?php
+
+$corEscolhida = "vermelho";
+
+switch ($corEscolhida) {
+    case "vermelho":
+        echo "Você escolher a cor: vermelha";
+        break;
+
+    case "azul";
+        echo "Você escolheu a cor: azul";
+        break;
+
+    case "verde";
+        echo "Você escolheu a cor: verde";
+        break;
+        
+    default:
+        echo "Cor não disponível";
+        break;  
+}
+?>
