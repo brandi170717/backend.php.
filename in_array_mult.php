4e3e69fd5c67ba18d5c79514ff6c9d6327143f14
brandi170717 <emanuelly.brandi.senai404@gmail.com>
2024-09-18 15:09:59 -0300

    Add files via upload

---
+++ in_array_mult.php
@@ -0,0 +1,17 @@
+<?php
+
+//
+$lista_frutas = ['Morango', 'Abacaxi', 'Maçã', 'Pera'];
+
+//
+$lista_coisas = [
+    'Frutas' => $lista_frutas,
+    'Pessoas' => ['João', 'Maria', 'Pedro']
+];
+
+//
+echo '<pre>';
+print_r($lista_coisas);
+echo '</pre<';
+
+?>
