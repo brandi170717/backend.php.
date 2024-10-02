4bb777eef1c1dc780b80bb0b3b0c3cdb49d5e92c
brandi170717 <emanuelly.brandi.senai404@gmail.com>
2024-10-02 16:04:38 -0300

    02/10

--- app_help_desk/valida_login.php
+++ app_help_desk/valida_login.php
@@ -1,12 +1,33 @@
 <?php
 
- echo "<pre>";
- print_r($_POST);
- echo "</pre>";
- echo "<hr>";
+//  echo "<pre>";
+//  print_r($_POST);
+//  echo "</pre>";
+//  echo "<hr>";
 
- echo "E-mail: ";
- echo $_POST['email'];
- echo '<br>';
-echo $_POST['senha'];
+//  echo "E-mail: ";
+//  echo $_POST['email'];
+//  echo '<br>';
+// echo $_POST['senha'];
+
+$usuario_autenticado = false;
+
+$usuario_cadestrado =[
+    ['email' => 'admi@senai.br',
+    'senha' => '12345' 
+    ]
+];
+
+foreach ($usuario_cadestrado as $user){
+    if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
+        $usuario_autenticado = true;
+    }
+}
+
+if ($usuario_autenticado == true){
+    echo "Usuário autenticado com sucesso";
+}else{
+    // echo"Usuário ou senha incorreto";
+    header ('Location: index.php?login=erro');
+}
 ?>
